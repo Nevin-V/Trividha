b42566859aec23dd65b56c4068878ccb76b517e8
Nevin v skaria <nevinvskaria@gmail.com>
2025-08-07 16:39:56 +0530

    fixing error in deployment

--- trividha/trividha/settings.py
+++ trividha/trividha/settings.py
@@ -150,6 +150,7 @@ STATICFILES_DIRS = [
 ]
 
 STATIC_ROOT = BASE_DIR / "staticfiles" 
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
